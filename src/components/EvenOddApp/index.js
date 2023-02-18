8147cdcbdd8a8b253b51fcb38c50a8403e946132
SAMBOJURAVITEJA <119179698+SAMBOJURAVITEJA@users.noreply.github.com>
2023-02-18 19:12:17 +0530

    Update index.js

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -1,49 +1,37 @@
-import {Component} from "react" 
+import {Component} from 'react'
 
-import "./index.css"            
-
+import './index.css'
 
 class EvenOddApp extends Component {
+  state = {text1: 0, text2: true}
 
+  change = () => {
+    this.setState(prevState => {
+      // Use the curly braces
+      const p = Math.ceil(Math.random() * 100)
 
+      if (p % 2 === 0) {
+        const k1 = true
+        return {text1: p, text2: k1}
+      }
+      const k1 = false // No need to use the else statement explicitly
+      return {text1: p, text2: k1}
+    })
+  }
 
+  render() {
+    const {text1, text2} = this.state
 
-
-
-    change=()=>{
-        this.setState((prevState)=>(
-             const p=Math.ceil(Math.random()*100)
-
-            if(p%2===0){
-                const k1=true
-                 return {text1:p,text2:k1}
-
-            }else{
-                const k1=false
-               return  {text1:p,text2:k1}
-            }
-            ))
-              
-             
-    }
-
-    state={text1:0,text2:true} 
-
-
-
-     render(){  
-
-        const{text1,text2}=this.state
-
-       return(<div class="bg-container">
-            <div class="sub-container">
-                <h1>Count {text1}</h1> 
-                 {text2?<p>Count is Even</p> : <p>Count is Odd</p> }
-                 <button onClick={this.change}>Increment</button>  
-                 <p>* Increase By Random Number Between 0 to 100</p>   
-            </div>
-        </div>)
-
-     }
+    return (
+      <div class="bg-container">
+        <div class="sub-container">
+          <h1>Count {text1}</h1>
+          {text2 ? <p>Count is Even</p> : <p>Count is Odd</p>}
+          <button onClick={this.change}>Increment</button>
+          <p>* Increase By Random Number Between 0 to 100</p>
+        </div>
+      </div>
+    )
+  }
 }
-default export EvenOddApp
+export default EvenOddApp
